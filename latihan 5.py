fbbd68525c17529accf84c63e26545284f4b2935
Agungmaulana29 <147120835+Agungmaulana29@users.noreply.github.com>
2023-10-19 21:24:41 -0700

    Add files via upload

---
+++ latihan 5.py
@@ -0,0 +1,7 @@
+nama = 'agung maulana'
+usia = '19'
+sudah_ menikah = true
+
+print('nama:',nama')
+print('usia:',usia)
+print('sudah menikah:'sudah_menikah)
---
+++ latihan 9.py
@@ -0,0 +1,13 @@
+nama_depan = "agung"
+nama_belakang ='maulana'
+nama_lengkap ='agung maulana'
+usia ='19'
+alamat ='pangkal_buluh'
+kata_mutiara='i_done_care'
+
+print(nama_lengkap,'('+usia')',',','dari',alamat,',kata _mutiara:,kata_mutiara)
+      
+print('\ntipe dari nama_lengkap:',type (nama_lengkap))
+print('tipe dari usia',type(usia))
+print('tipe dari alamat;', type(alamat))
+print('tipe darikata_mutiara:',type(kata_mutiara))
